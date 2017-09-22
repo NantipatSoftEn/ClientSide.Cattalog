4f341044c4355c6f0c9539d771ddb29803c7c629
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-09-22 10:54:04 +0700

    add function get api

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import logo from './logo.svg';
 
 
 class App extends Component {
+componentDidMount() {
+   fetch('http://127.0.0.1:5000/api/v1/pages')
+     .then((response) => response.json())
+     .then((pages) => this.setState({ pages }))
+ }
   render() {
     return (
       <div className="container">
