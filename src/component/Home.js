08b94ec4eb3402bc64e22f7ea88d2436f62b4305
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-09-27 10:27:25 +0700

    npm install upload file

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react'
-import { FormGroup,ControlLabel,HelpBlock,FormControl,Grid,Row} from 'react-bootstrap';
+import { FormGroup,ControlLabel,HelpBlock,FormControl,Grid,Row,Col,Input} from 'react-bootstrap';
 class Home extends Component {
   render() {
     return (
         <div>
             <Grid>
                 <Row>
-                    <form action=''> 
-                        <FormGroup controlId="formValidationSuccess1" validationState="success">
-                            <ControlLabel>Input with success</ControlLabel>
-                            <FormControl type="text" />
-                            <HelpBlock>Help text with validation state.</HelpBlock>
+
+                    <Col xs={6} md={7}>
+                        <form action='http://localhost:3001/c' method='post'>
+                        <FormGroup>
+
                         </FormGroup>
-                    </form>
+                            <FormGroup>
+                                <FormControl type="text" name='name'placeholder="Enter Name"/>
+                            </FormGroup>
+                            <FormGroup>
+                                <FormControl type="text" name='facebook'placeholder="Enter facebook"/>
+                            </FormGroup>
+                            <FormGroup>
+                                <FormControl type="text" name='rank'placeholder="Enter rank"/>
+                                <HelpBlock>Number 0-10000</HelpBlock>
+                            </FormGroup>
+                             <input type="submit" value="Submit" bsStyle="success"/>
+                        </form>
+                    </Col>
                 </Row>
             </Grid>
        </div>
