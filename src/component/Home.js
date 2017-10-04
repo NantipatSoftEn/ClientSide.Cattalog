8c897ca8d40d1d73fa44ce3f8dae579542504517
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-10-04 13:51:11 +0700

    remove reactstrap

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 
-import { FormGroup,HelpBlock,FormControl,Grid,Row,Col,Button} from 'react-bootstrap';
 class Home extends Component {
   render() {
     return (
@@ -9,21 +8,7 @@ class Home extends Component {
                 <Row>
 
                     <Col xs={6} md={7}>
-                        <form onSubmit={this.handleSubmit} method='post'>
-                        <FormGroup>
-                        <FormControl type="file" name='img' />
-                        </FormGroup>
-                            <FormGroup>
-                                <FormControl type="text" name='name'placeholder="Enter Name" bsSize="large"/>
-                            </FormGroup>
-                            <FormGroup>
-                                <FormControl type="text" name='facebook'placeholder="Enter facebook" bsSize="large"/>
-                            </FormGroup>
-                            <FormGroup>
-                                <FormControl type="text" name='rank'placeholder="Enter rank" bsSize="large"/>
-                                <HelpBlock>Number 0-10000</HelpBlock>
-                            </FormGroup>
-                             <Button bsStyle="success" type="submit">Success</Button>
+                        
                         </form>
                     </Col>
                 </Row>
