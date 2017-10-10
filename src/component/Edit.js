0d69ad1688550f61b63610bb365dde688c104926
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-10-10 11:49:23 +0700

    show value sucess

--- src/component/Edit.js
+++ src/component/Edit.js
@@ -5,39 +5,30 @@ import { Button,Grid,Row,Col,Thumbnail} from 'react-bootstrap';
 class Edit extends Component {
     constructor(props) {
         super(props);
-
+        console.log(this.props.params.id);
+        this.state({gg:''});
     }
     state = {
         pages: [],
   }
+
     componentDidMount() {
       fetch('http://localhost:3001/c/'+this.props.params.id)
         .then(response => {
             return response.json()
         })
         .then(pages =>
+            //console.log(pages)
             this.setState( { pages } )
         )
+
     }
   render() {
     return (
         <div>
             <Grid>
                 <Row>
-                {
-                this.state.pages.map((page) => (
-                    <Col xs={6} md={4} >
-                            <Thumbnail src={page.img}  alt="242x200">
-                                <a href={page.facebook}>
-                                    <h3>{page.name}</h3>
-                                 </a>
-                                <p>
-                                    <Button bsStyle="success"> RanK: {page.rank}</Button>
-                                </p>
-                            </Thumbnail>
-                    </Col>
-                    ))
-                }
+                {this.state.pages.name}
                 </Row>
             </Grid>
         </div>
