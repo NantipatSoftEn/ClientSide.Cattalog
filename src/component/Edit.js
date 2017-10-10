233e39c631f727535b9b827e52cc61450e591dcf
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-10-10 13:51:40 +0700

    form edit

--- src/component/Edit.js
+++ src/component/Edit.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import { Button,Grid,Row,Col,Thumbnail} from 'react-bootstrap';
-
+import { Button,Grid,Row,Col,FormGroup,Image,FormControl,InputGroup} from 'react-bootstrap';
+import AlertContainer from 'react-alert'
 const initialState = { person:[]};
 class Edit extends Component {
     constructor(props) {
@@ -23,11 +23,44 @@ class Edit extends Component {
         <div>
             <Grid>
                 <Row>
-                    {this.state.person.name}
+                <Col xs={6} md={6} xsOffset={3}>
+                    <Image src={this.state.person.img} responsive />
+                    <form onSubmit={this.handleSubmit} >
+                    <FormGroup bsSize="large">
+                        <InputGroup>
+                            <InputGroup.Addon>Name</InputGroup.Addon>
+                                <FormControl type="text" ref='name' name='name'
+                                value={this.state.name}
+                                onChange={this.handleChange}  />
+                        </InputGroup>
+                    </FormGroup>
+                    <FormGroup bsSize="large">
+                        <InputGroup>
+                            <InputGroup.Addon>Facebook</InputGroup.Addon>
+                                <FormControl type="text" ref='facebook' name='facebook'
+                                value={this.state.facebook}
+                                onChange={this.handleChange}  />
+                        </InputGroup>
+                    </FormGroup>
+                    <FormGroup bsSize="large">
+                        <InputGroup>
+                            <InputGroup.Addon>Rank</InputGroup.Addon>
+                                <FormControl type="number" min="-10" max="99"
+                                 ref='rank' name='rank'
+                                 value={this.state.rank}
+                                 onChange={this.handleChange}  />
+                        </InputGroup>
+                    </FormGroup>
+                    <AlertContainer ref={a => this.msg = a} {...this.alertOptions} />
+                                <center>
+                                    <Button type="submit" bsStyle="success" onClick={this.showAlert}>เก็บรูปในคลัง</Button>
+                                </center>
+                    </form>
+                </Col>
                 </Row>
             </Grid>
         </div>
-    )
+    );
   }
 }
 export default Edit
