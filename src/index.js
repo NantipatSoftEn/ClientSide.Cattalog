77a985ce68f7ade72505498f28e644fe3d7a869a
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-10-12 15:29:03 +0700

    spite Form

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Pages from './component/Pages';
 import Edit from './component/Edit';
 import Delete from './component/Delete';
 
-import Upload from './component/Upload';
+import FormCon from './component/FormCon';
 import {
   Router,
   Route,
@@ -24,7 +24,7 @@ ReactDOM.render(
      <Route path='/Edit/:id' component={Edit} />
      <Route path='/Delete' component={Delete} />
 
-     <Route path='/test' component={Upload} />
+     <Route path='/test' component={FormCon} />
    </Route>
  </Router>
 , document.getElementById('root'));
