b03b1fbf7a8d323f8630b16897787fdc4c271c8e
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-10-16 10:50:58 +0700

    in component have state  cant send props

--- src/component/Edit.js
+++ src/component/Edit.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import { Button,Grid,Row,Col,FormGroup,Image,FormControl,InputGroup} from 'react-bootstrap';
-import AlertContainer from 'react-alert'
+import { Grid,Row,Col,Image} from 'react-bootstrap';
+import FormContainer from './FormContainer'
 const initialState = { person:[]};
 class Edit extends Component {
     constructor(props) {
@@ -25,38 +25,11 @@ class Edit extends Component {
                 <Row>
                 <Col xs={6} md={6} xsOffset={3}>
                     <Image src={this.state.person.img} responsive />
-                    <form onSubmit={this.handleSubmit} >
-                    <FormGroup bsSize="large">
-                        <InputGroup>
-                            <InputGroup.Addon>Name</InputGroup.Addon>
-                                <FormControl type="text" ref='name' name='name'
-                                value={this.state.name}
-                                onChange={this.handleChange}  />
-                        </InputGroup>
-                    </FormGroup>
-                    <FormGroup bsSize="large">
-                        <InputGroup>
-                            <InputGroup.Addon>Facebook</InputGroup.Addon>
-                                <FormControl type="text" ref='facebook' name='facebook'
-                                value={this.state.facebook}
-                                onChange={this.handleChange}  />
-                        </InputGroup>
-                    </FormGroup>
-                    <FormGroup bsSize="large">
-                        <InputGroup>
-                            <InputGroup.Addon>Rank</InputGroup.Addon>
-                                <FormControl type="number" min="-10" max="99"
-                                 ref='rank' name='rank'
-                                 value={this.state.rank}
-                                 onChange={this.handleChange}  />
-                        </InputGroup>
-                    </FormGroup>
-                    <AlertContainer ref={a => this.msg = a} {...this.alertOptions} />
-                                <center>
-                                    <Button type="submit" bsStyle="success" onClick={this.showAlert}>เก็บรูปในคลัง</Button>
-                                </center>
-                    </form>
                 </Col>
+                <FormContainer
+                name={this.state.person.name}
+                facebook={this.state.person.facebook}
+                rank={this.state.person.rank} />
                 </Row>
             </Grid>
         </div>
