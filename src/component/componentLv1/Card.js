e44e26a5a55475bb629828a355ad0395c19df681
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-10-18 10:53:00 +0700

    bug del item

--- src/component/componentLv1/Card.js
+++ src/component/componentLv1/Card.js
@@ -3,29 +3,36 @@ import React, { Component } from 'react'
 import { Button,Thumbnail} from 'react-bootstrap';
 import { Link } from 'react-router'
 class Card extends Component {
-    deleteData(id) {
-      fetch('http://localhost:3001/c'+ id, {
-          method: 'DELETE',
-          headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json',
-          }
-      }).then(response => response.json())
-    }
+    // deleteData(id) {
+    //
+    //   fetch('http://localhost:3001/c/'+ id, {
+    //       method: 'DELETE',
+    //       headers: {
+    //           'Accept': 'application/json',
+    //           'Content-Type': 'application/json',
+    //       }
+    //   }).then(   response => response.json())
+    // }
+
+    /*
+     Bug Function
+    */
   render() {
+      console.log(this.props.id);
+
     return (
 
         <Thumbnail src={this.props.img}  alt="242x200">
             <a href={this.props.facebook}>
                 <h3>{this.props.name}</h3>
+
              </a>
             <p>
             <Link to={{ pathname: `/Edit/${ this.props.id }` }}
             className="btn btn-primary" >
                 Edit
             </Link> &nbsp;
-
-            <Button bsStyle="danger" onClick={this.deleteData.bind(this.props.id)}> Delete</Button> &nbsp;
+            <Button bsStyle="danger" > Delete</Button> &nbsp;
 
             <Button bsStyle="success"> RanK: {this.props.rank}</Button>
             </p>
