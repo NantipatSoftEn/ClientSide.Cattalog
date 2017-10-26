cd472586e1f85af1cca41b46a5b622cfe5113220
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-10-26 22:36:13 +0700

    :memo::memo::memo:

--- src/component/Upload.js
+++ src/component/Upload.js
@@ -1,6 +1,12 @@
-import React, { Component } from 'react'
+import React, {
+  Component
+} from 'react'
 import request from 'superagent';
-import { Grid,Row,Col} from 'react-bootstrap';
+import {
+  Grid,
+  Row,
+  Col
+} from 'react-bootstrap';
 
 import ImageUploader from 'react-images-upload';
 
@@ -10,58 +16,82 @@ const CLOUDINARY_UPLOAD_PRESET = 'd6bx32ar';
 const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/soften57/upload';
 
 const initialState = {
-    uploadedFile:null,
-    uploadedFileCloudinaryUrl:'',
+  uploadedFile: null,
+  uploadedFileCloudinaryUrl: '',
 };
 class Upload extends Component {
-    constructor(props) {
-        super(props);
-        this.state = initialState;
-    }
-    onImageDrop(files) {
-            console.log(files);
-            this.setState({uploadedFile: files[0]});
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
+  onImageDrop(files) {
+    console.log(files);
+    this.setState({
+      uploadedFile: files[0]
+    });
 
-        this.handleImageUpload(files[0]);
-    }
+    this.handleImageUpload(files[0]);
+  }
 
-    handleImageUpload(file) {
-        let upload = request.post(CLOUDINARY_UPLOAD_URL)
-                         .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
-                         .field('file', file);
+  handleImageUpload(file) {
+    let upload = request.post(CLOUDINARY_UPLOAD_URL)
+      .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+      .field('file', file);
 
-        upload.end((err, response) => {
-          if (err) {
-            console.error(err);
-          }
+    upload.end((err, response) => {
+      if (err) {
+        console.error(err);
+      }
 
-          console.log(response.body.secure_url);
-          if (response.body.secure_url !== '') {
-            this.setState({
-              uploadedFileCloudinaryUrl: response.body.secure_url
-            });
-          }
+      console.log(response.body.secure_url);
+      if (response.body.secure_url !== '') {
+        this.setState({
+          uploadedFileCloudinaryUrl: response.body.secure_url
+        });
+      }
     });
   }
   render() {
-    return (
-        <div>
-            <Grid>
-                <Row>
-                    <Col xs={6} md={7} xsOffset={3}>
-                    <ImageUploader
-                        withIcon={true}
-                        buttonText='Choose images'
-                        fileSizeError='fileSizeError'
-                        onChange={this.onImageDrop.bind(this)}
-                        imgExtension={['.jpg', '.gif', '.png', '.gif']}
-                        maxFileSize={5242880}
-                    />
-                    </Col>
-                    <FormContainer upload={this.state.uploadedFileCloudinaryUrl}/>
-                </Row>
-            </Grid>
-       </div>
+    return ( <
+      div >
+      <
+      Grid >
+      <
+      Row >
+      <
+      Col xs = {
+        6
+      }
+      md = {
+        7
+      }
+      xsOffset = {
+        3
+      } >
+      <
+      ImageUploader withIcon = {
+        true
+      }
+      buttonText = 'Choose images'
+      fileSizeError = 'fileSizeError'
+      onChange = {
+        this.onImageDrop.bind(this)
+      }
+      imgExtension = {
+        ['.jpg', '.gif', '.png', '.gif']
+      }
+      maxFileSize = {
+        5242880
+      }
+      /> <
+      /Col> <
+      FormContainer upload = {
+        this.state.uploadedFileCloudinaryUrl
+      }
+      /> <
+      /Row> <
+      /Grid> <
+      /div>
     )
   }
 }
